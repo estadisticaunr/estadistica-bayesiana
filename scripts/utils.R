1abfc5eddb30f3209188f3bf5936f9acc5bb6403
Tomas Capretto <tomicapretto@gmail.com>
2023-03-09 12:01:49 -0300

    Agrego ejercicio mesquite

--- scripts/utils.R
+++ scripts/utils.R
@@ -1,6 +1,6 @@
 make_unsplash_caption <- function(author, handle, code) {
     template <- paste(
-        "Foto de <a href='https://unsplash.com/%s'>%s</a>", 
+        "Foto de <a href='https://unsplash.com/%s'>%s</a>",
         "en <a href='https://unsplash.com/photos/%s'>Unsplash</a>"
     )
     return(sprintf(template, handle, author, code))
